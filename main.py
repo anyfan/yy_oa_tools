730d2c94f1c1c4f8ac3c80b90c08b5c1dab6ac64
yyp8 <yangyiping@tianqing.space>
2024-06-12 09:00:59 +0800

    新增处理合并单元格

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ from copy import deepcopy
 from docx import Document
 from string import Template
 
+pd.set_option('future.no_silent_downcasting', True)   # 设置选项以启用未来版本的行为
+
 # 资源文件打包到exe时，生成资源文件的访问路径
 def resource_path(relative_path):
-    if getattr(sys, 'frozen', False): #是否Bundle Resource
+    if getattr(sys, 'frozen', False): # 是否Bundle Resource
         base_path = sys._MEIPASS
     else:
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
+
+def fill_merged_cells(df):
+    for col in df.columns:
+        df[col] = df[col].ffill().infer_objects(copy=False)
+    return df
 
 # 清理字符串，去除换行符
 def clean_string(input_string):
@@ -21,9 +28,8 @@ def clean_string(input_string):
     else:
         return input_string
 
-def add_table(docx_obj, mb_obj, data_obj): 
-
-# 数据对象，用于填充模板 
+def add_table(docx_obj, mb_obj, data_obj):
+    # 数据对象，用于填充模板 
     data = {
         'dybs'  : data_obj['单元标识'],
         'sjzs'  : data_obj['设计追踪'],
@@ -36,8 +42,8 @@ def add_table(docx_obj, mb_obj, data_obj):
         'csylbs': data_obj['测试用例标识'],
         'cslx'  : data_obj['测试类型'],
         'hdz'   : data_obj['活动桩'],
-        'cryj'  :  '\n' + data_obj['插入语句'] if '\n' in data_obj['插入语句'] else data_obj['插入语句'] ,
-        'qjbl'  :  '\n' + data_obj['创建全局变量'] if '\n' in data_obj['创建全局变量'] else data_obj['创建全局变量'] ,
+        'cryj'  : '\n' + data_obj['插入语句'] if isinstance(data_obj['插入语句'], str) and '\n' in data_obj['插入语句'] else data_obj['插入语句'],
+        'qjbl'  : '\n' + data_obj['创建全局变量'] if isinstance(data_obj['创建全局变量'], str) and '\n' in data_obj['创建全局变量'] else data_obj['创建全局变量'],
         'cssm'  : data_obj['测试说明'],
         'srblm' : data_obj['输入变量名称'],
         'srblqz': data_obj['取值'],
@@ -49,8 +55,8 @@ def add_table(docx_obj, mb_obj, data_obj):
         'csry'  : data_obj['测试者/校对者'],
         'cszxsj': data_obj['测试时间'].date() if isinstance(data_obj['测试时间'], pd.Timestamp) else data_obj['测试时间'],
     }
-  
-# 如果测试者/校对者字段包含'/'分隔符，则分别赋值给相应的变量
+
+    # 如果测试者/校对者字段包含'/'分隔符，则分别赋值给相应的变量
     cs_jz = str(data_obj['测试者/校对者'])
     if '/' in cs_jz:
         data['sjz'] = cs_jz.split('/')[0]
@@ -61,10 +67,10 @@ def add_table(docx_obj, mb_obj, data_obj):
         data['xdz'] = cs_jz
         data['csry'] = cs_jz
 
-# 复制模板中的表格
+    # 复制模板中的表格
     new_table = deepcopy(mb_obj.tables[0])
 
-# 填充表格中的文本内容    
+    # 填充表格中的文本内容    
     for row in new_table.rows:
         for cell in row.cells:
             for paragraph in cell.paragraphs:
@@ -72,32 +78,33 @@ def add_table(docx_obj, mb_obj, data_obj):
                     run_template = Template(run.text)
                     run.text = run_template.substitute(data)
 
-# 在文档中添加新表格                        
+    # 在文档中添加新表格                        
     paragraph = docx_obj.add_paragraph(f"{data_obj['测试用例名称']}")
     paragraph._p.addnext(new_table._element)
     docx_obj.add_page_break()
+    
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:                              # 检查命令行参数是否正确
+    if len(sys.argv) != 2:  # 检查命令行参数是否正确
         print('\033[0;31mneed excel file\033[0m')
         os.system("pause")
         sys.exit(1)
+        
     excel_file = sys.argv[1]
-    # excel_file = 'dist/单元测试协同表格.xlsx'
-    excel_datas = pd.read_excel(excel_file,sheet_name=None)             # 读取Excel文件中的数据，读取后对 DataFrame 进行转换
-    excel_datas = {sheet_name: df.astype(str) for sheet_name, df in excel_datas.items()}
+    excel_datas = pd.read_excel(excel_file, sheet_name=None, dtype=str)
+    excel_datas = {sheet_name: fill_merged_cells(df) for sheet_name, df in excel_datas.items()} # 处理合并单元格
 
-    template_docx = Document(resource_path("template/template.docx"))    # 加载Word文档模板
-    new_docx = Document(resource_path("template/new.docx"))   # 创建新的Word文档对象
+    template_docx = Document(resource_path("template/template.docx"))  # 加载Word文档模板
+    new_docx = Document(resource_path("template/new.docx"))  # 创建新的Word文档对象
 
     template_count = 0  # 初始化模板计数器
     
-    for sheet_name, df in excel_datas.items():                           # 遍历Excel数据，生成测试文档
+    for sheet_name, df in excel_datas.items():  # 遍历Excel数据，生成测试文档
         if sheet_name in ['test', '索引']:
             continue
         for index, row in df.iterrows():
             print(sheet_name, row['测试用例名称'])
-            add_table(new_docx,template_docx,row)
+            add_table(new_docx, template_docx, row)
             template_count += 1  # 每成功填充一次模板，计数器加一
 
     save_file_name = os.path.splitext(excel_file)[0] + '_oa.docx'
@@ -107,5 +114,3 @@ if __name__ == '__main__':
     print(f"\033[0;32m一共执行了{template_count}个测试用例\033[0m")  # 打印生成的用例次数
 
     os.system("pause")
-
-# ☑☐□
